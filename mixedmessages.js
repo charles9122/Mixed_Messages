87b0ab646cf36a65a5a07abfd5ce3d2d978fe6da
charles9122 <89087887+charles9122@users.noreply.github.com>
2021-08-23 21:03:39 +0100

    added function to mixed messages songs array

--- mixedmessages.js
+++ mixedmessages.js
@@ -2,3 +2,10 @@ const rock = ["American Idiot by Green Day", "The Middle by Jimmy Eat World", "L
 const pop = ["Poker Face by Lady Gaga", "Halo by Beyonce", "Sk8er Boi by Avril Lavigne", "So What by Pink", "Umbrella by Rihanna", "Bye Bye Bye by *NSYNC", "I Kissed a Girl by Katy Perry", "Fix You by Coldplay", "Dnacing in the Moonlight by Toploader", "Marry You by Bruno Mars", "Love Story by Taylor Swift", "Bad Habits by Ed Sheeran"];
 const rap = ["Still D.R.E. by Dr. Dre and Snoop Dogg", "In Da Club by 50 Cent", "Ms. Jackson by Outkast", "Black and Yellow by Wiz Khalifa", "X Gon' Give It To Ya by DMX", "The Real Slim Shady by Eminem", "Straight Outta Compton by N.W.A.", "Gold Digger by Kanye West", "Ghetto Gospel by 2Pac", "WAP by Cardi B", "Gangsta's Paradise by Coolio", "Ni**as In Paris by JAY-Z and Kanye West"];
 const indie = ["Mardy Bum by Arctic Monkeys", "Amsterdam by Nothing But Thieves", "Naive by The Kooks", "Ruby by Kaiser Chiefs", "Take Me Out by Franz Ferdinand", "Club Foot by Kasabian", "Wonderwall by Oasis", "Chocolate by The 1975", "I Will Wait by Mumford and Sons", "Zombie by Jamie T", "I Found Out by The Pigeon Detectives", "In The Morning by Razorlight"];
+
+function generateRandomNumber(num) {
+    // Gets # from 0 -> num - 1
+    return Math.floor(Math.random() * num)
+  }
+   
+// Gap for the Array of genres and songs // 
